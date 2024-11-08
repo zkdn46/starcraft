a91720601eaa819b06f6df0462ca668a0f45163a
zkdn46 <123gusckd123@naver.com>
2024-11-08 15:52:54 +0900

    Add Tank Attackable

--- src/starcraft/Tank.java
+++ src/starcraft/Tank.java
@@ -1,7 +1,11 @@
 package starcraft;
 
-public class Tank extends GroundUnit implements Repairable {
+public class Tank extends GroundUnit implements Repairable, Attackable{
 	protected Tank() {
 		super("탱크", 300);
+	}
+
+	public void attack(Unit unit) {
+		unit.hp-=30;
 	}
 }
