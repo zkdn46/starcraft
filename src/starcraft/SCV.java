867117af9c7e59efa8ab05cb92651c3db61c1156
zkdn46 <123gusckd123@naver.com>
2024-11-08 16:50:17 +0900

    Fix SCV

--- src/starcraft/SCV.java
+++ src/starcraft/SCV.java
@@ -8,21 +8,24 @@ public class SCV extends GroundUnit implements Repairable, Attackable {
 	protected void repair(Repairable unit) {
 		Unit target = (Unit) unit;
 
-		while (target.hp!=target.MAX_HP) {
-			target.hp++;
-			String msg = String.format("%s 수리중 [%d/%d]", target.name, target.hp, target.MAX_HP);
-			System.out.println(msg);
+		if (target.hp == target.MAX_HP) {
+			System.out.println("수리 할 HP가 없습니다.");
+		} else {
+			while (target.hp != target.MAX_HP) {
+				target.hp++;
+				String msg = String.format("%s 수리중 [%d/%d]", target.name, target.hp, target.MAX_HP);
+				System.out.println(msg);
 
-			try {
-				Thread.sleep(200);
-			} catch (Exception e) {
+				try {
+					Thread.sleep(200);
+				} catch (Exception e) {
+				}
 			}
+			System.out.println("수리 완료");
 		}
-		System.out.println("수리 완료");
-
 	}
 
 	public void attack(Unit unit) {
-		unit.hp-=5;
+		unit.hp -= 5;
 	}
 }
