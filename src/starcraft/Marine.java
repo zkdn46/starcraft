553dc256583b04eaff8b46dabe98c71bfeb698e9
zkdn46 <123gusckd123@naver.com>
2024-11-08 15:52:42 +0900

    Add Marine Attackable

--- src/starcraft/Marine.java
+++ src/starcraft/Marine.java
@@ -1,7 +1,11 @@
 package starcraft;
 
-public class Marine extends GroundUnit {
+public class Marine extends GroundUnit implements Attackable{
 	protected Marine() {
 		super("마린", 120);
+	}
+	
+	public void attack(Unit unit) {
+		unit.hp-=15;
 	}
 }
